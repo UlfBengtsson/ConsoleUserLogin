1fc9bbd571167b9a884003ff401eea3c806b358a
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-23 08:41:34 +0200

    Test updated

--- UserLogin.Tests/UserLoginManagerTests.cs
+++ UserLogin.Tests/UserLoginManagerTests.cs
@@ -3,12 +3,24 @@ using Xunit;
 
 namespace UserLogin.Tests
 {
+    /*
+    1: Password must have a letter and a didiget(number).
+    2: Username is unique(only one).
+    3: Can only login with a excisting user and there password must match.
+    */
     public class UserLoginManagerTests
     {
         [Fact]
-        public void Test1()
+        public void TestTemplate()
         {
+            //Arrange
+            
 
+            //Act
+            
+
+            //Assert
+            
         }
     }
 }
